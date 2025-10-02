935a1e7f26ca5655dd0ee6b108274815c6e4536e
Denis Koenig <kyoni9d@gmail.com>
2025-10-02 03:08:57 +0300

    feat: add Conference model.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Speaker(models.Model):
@@ -8,3 +9,27 @@ class Speaker(models.Model):
 
     def __str__(self):
         return f'{self.full_name} — "{self.topic}"'
+
+
+class Conference(models.Model):
+    speaker = models.ForeignKey(
+        Speaker,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Текущий докладчик",
+    )
+    start_time = models.DateTimeField(
+        null=True, blank=True, verbose_name="Время старта"
+    )
+    is_running = models.BooleanField(default=False, verbose_name="Таймер запущен")
+    extra_time = models.IntegerField(
+        default=0, verbose_name="Дополнительное время (сек)"
+    )
+
+    def calculate_remaining_time(self):
+        if not self.speaker or not self.start_time:
+            return None
+        total_seconds = self.speaker.duration * 60 + self.extra_time
+        elapsed = (timezone.now() - self.start_time).total_seconds()
+        return int(total_seconds - elapsed)
