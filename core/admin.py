d42a95e016540c085b4f0ba26873fedc22ca9079
Denis Koenig <kyoni9d@gmail.com>
2025-10-03 01:31:48 +0300

    chore: register core models in Django admin.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,27 @@
+from django import forms
 from django.contrib import admin
+from .models import Speaker, Conference
 
-# Register your models here.
+
+@admin.register(Speaker)
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "topic", "duration")
+    search_fields = ("full_name", "topic")
+
+
+@admin.register(Conference)
+class ConferenceAdmin(admin.ModelAdmin):
+    list_display = ("speaker", "start_time", "is_running", "extra_time")
+    list_editable = ("is_running", "extra_time")
+
+    def has_add_permission(self, request):
+        if Conference.objects.exists():
+            return False
+        return super().has_add_permission(request)
+
+
+class ConferenceForm(forms.ModelForm):
+    class Meta:
+        model = Conference
+        fields = "__all__"
+        widgets = {"start_time": forms.TimeInput(format="%H:%M")}
